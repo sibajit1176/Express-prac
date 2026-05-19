5dc4ed495cac3428bef592453ee7629930829afc
sibajit1176 <sibajitmaji2002@gmail.com>
2026-05-19 11:44:53 +0530

    wildcard route

--- main.js
+++ main.js
@@ -19,6 +19,9 @@ app.get("/users",(req,res)=>{
 app.post("/users",(req,res)=>{
     res.send("A new user has been added.")
 })
+app.use((req,res)=>{
+    res.status(404).send('<h1>404 - Page Not Found</h1>')
+})
 
 app.listen(Port,()=>{
  console.log(`Server is running on http://localhost:${Port}`);
