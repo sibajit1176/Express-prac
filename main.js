81552314a81c6dd6454509754133218e68ae20c2
sibajit1176 <sibajitmaji2002@gmail.com>
2026-05-19 11:00:12 +0530

    middleware

--- main.js
+++ main.js
@@ -2,6 +2,16 @@ const express=require('express')
 const app=express()
 const Port=3000
 
+const addUser=(req,res,next)=>{
+    req.user='Guest'
+    next()
+}
+
+
+app.get('/welcome',addUser,(req,res)=>{
+    res.send(`<h1>Welcome ,${req.user}</h1>`)
+})
+
 app.listen(Port,()=>{
  console.log(`app run on ${Port}`);
  
