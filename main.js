4c9f9a43042947ccefc03b8a27bedfab7a68b2f2
sibajit1176 <sibajitmaji2002@gmail.com>
2026-05-19 12:53:22 +0530

    books route

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const app=express()
 const userRouter=require('./routes/userRoutes')
 const orderRouter=require('./routes/orderRoutes')
 const dynamicRouter=require('./routes/dynamicRoutes')
+const libreryrouter=require('./libreryRoutes/bookRoutes')
 const Port=3000
 
 app.use((req,res,next)=>{
@@ -12,6 +13,7 @@ app.use((req,res,next)=>{
 app.use('/orders',orderRouter)
 app.use('/users',userRouter)
 app.use('/Wellcome',dynamicRouter)
+app.use('/books',libreryrouter)
 
 app.use((req,res)=>{
     res.status(404).send('<h1>404 - Page Not Found</h1>')
