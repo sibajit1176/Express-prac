9af35b24a569dd20f6a90dccf0066d86aee9ce65
sibajit1176 <sibajitmaji2002@gmail.com>
2026-05-19 12:18:10 +0530

    git push

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const app=express()
 const Port=3000
 
 app.use((req,res,next)=>{
-    console.log("User logged in");
-    next()
+    console.log(`${req.method} request made to ${req.url}`);
+    next();
 })
 
 app.get("/orders",(req,res)=>{
